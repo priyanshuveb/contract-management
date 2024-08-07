d68d91d65d239556ab8c019963aa39d07360984b
priyanshuveb <priyanshubindal@gmail.com>
2024-08-07 07:34:40 +0530

    first commit

---
+++ src/ContractManagement.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+import {AccessControl} from "./AccessControl.sol";
+contract ContractManagement is AccessControl {
+    // uint256 public number;
+
+    // function setNumber(uint256 newNumber) public {
+    //     number = newNumber;
+    // }
+
+    // function increment() public {
+    //     number++;
+    // }
+}
